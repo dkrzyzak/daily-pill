e4fb90a0494c94bb8858cc64eb32a1366c6cfa19
dkrzyzak <damntheguitars@gmail.com>
2025-02-26 22:08:02 +0100

    update packages before adding new service

--- api/api-gateway/src/main.ts
+++ api/api-gateway/src/main.ts
@@ -4,5 +4,9 @@ import cookieParser from 'cookie-parser';
 
 NestFactory.create(AppModule).then((app) => {
   app.use(cookieParser());
+  // app.enableCors({
+  //   origin: process.env.FRONTEND_URL,
+  //   credentials: true, // Important!
+  // });
   app.listen(process.env.PORT ?? 3000);
 });
