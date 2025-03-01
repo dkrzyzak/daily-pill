42791e5cd32cac68ccc031d9d54793f9b234813d
dkrzyzak <damntheguitars@gmail.com>
2025-03-01 21:01:23 +0100

    fix api gateway eating away request body

--- api/api-gateway/src/main.ts
+++ api/api-gateway/src/main.ts
@@ -3,10 +3,10 @@ import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 
 NestFactory.create(AppModule).then((app) => {
-  app.use(cookieParser());
-  // app.enableCors({
-  //   origin: process.env.FRONTEND_URL,
-  //   credentials: true, // Important!
-  // });
-  app.listen(process.env.PORT ?? 3000);
+    app.use(cookieParser());
+    app.enableCors({
+        origin: true,
+        credentials: true,
+    });
+    app.listen(process.env.PORT ?? 3000);
 });
--- api/api-gateway/src/proxy.controller.ts
+++ api/api-gateway/src/proxy.controller.ts
@@ -1,7 +1,11 @@
 import { All, Controller, NotFoundException, Req, Res } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Request, Response } from 'express';
-import { createProxyMiddleware, RequestHandler } from 'http-proxy-middleware';
+import {
+    createProxyMiddleware,
+    RequestHandler,
+    fixRequestBody,
+} from 'http-proxy-middleware';
 
 @Controller()
 export class ProxyController {
@@ -13,11 +17,20 @@ export class ProxyController {
                 return this.getServiceUrl(req.path);
             },
             changeOrigin: true,
+            on: {
+                proxyReq: fixRequestBody,
+            },
         });
     }
 
     @All('*')
     async proxy(@Req() req: Request, @Res() res: Response) {
+        const target = this.getServiceUrl(req.path);
+
+        if (!target) {
+            throw new NotFoundException('Unknown service');
+        }
+
         this.proxyMiddleware(req, res, (err) => {
             if (err) {
                 console.log(err);
@@ -26,7 +39,7 @@ export class ProxyController {
         });
     }
 
-    private getServiceUrl(path: string): string {
+    private getServiceUrl(path: string): string | undefined {
         if (path.startsWith('/auth')) {
             return this.configService.get<string>('AUTH_SERVICE_URL')!;
         }
@@ -39,6 +52,6 @@ export class ProxyController {
             return this.configService.get<string>('SCHEDULE_SERVICE_URL')!;
         }
 
-        throw new NotFoundException('Unknown service');
+        return undefined;
     }
 }
